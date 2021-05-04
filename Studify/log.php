09a84a640ee7cd63249914e964d730d05f2a63ef
Maxi968 <massimiliano.baldassi.968@gmail.com>
2021-05-04 20:31:22 +0200

    Roba lato server
    Mo si connette al database, ma non salva i dati. Domani risolvo

--- Studify/log.php
+++ Studify/log.php
@@ -1,45 +1,41 @@
-<?php
-session_start();
-require_once('database.php');
-
-if (isset($_SESSION['session_id'])) {
-    header('Location: dashboard.php');
-    exit;
-}
-
-if (isset($_POST['login'])) {
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
-    $email = $_POST['email'] ?? '';
-    $nome = $_POST['nome'] ?? '';
-    $cognome = $_POST['cognome'] ?? '';
-
-    if (empty($username) || empty($password)) {
-        $msg = 'Inserisci username e password %s';
-    } else {
-        $query = "
-            SELECT username, password
-            FROM users
-            WHERE username = :username
-        ";
+<html>
+    <head></head>
+    <body>
+    <?php
+        $dbconn= pg_connect("host=localhost port=5432
+            dbname=StudifyDB
+            user=postgres password=password")
+            or die('Impossibile connettersi: '.pg_last_error());
+        echo "Connessione riuscita.";
         
-        $check = $pdo->prepare($query);
-        $check->bindParam(':username', $username, PDO::PARAM_STR);
-        $check->execute();
-        
-        $user = $check->fetch(PDO::FETCH_ASSOC);
-        
-        if (!$user || password_verify($password, $user['password']) === false) {
-            $msg = 'Credenziali utente errate %s';
-        } else {
-            session_regenerate_id();
-            $_SESSION['session_id'] = session_id();
-            $_SESSION['session_user'] = $user['username'];
-            
-            header('Location: dashboard.php');
-            exit;
+        if (!(isset($_POST['loginButton']))) {
+            header('Location: index.html');
         }
-    }
-    
-    printf($msg, '<a href="../login.html">torna indietro</a>');
-}
+        else {
+            $email = $_POST["email"];
+            $q1 = "SELECT * from utenti where email=$1";
+            $result = pg_query_params($dbconn, $q1, array($email));
+            if (!($line=pg_fetch_array($result,null,PGSQL_ASSOC))) {
+                echo "<h1> Non sei ancora registrato!</h1>
+                    <a href= registrazione.html>
+                    Clicca qui per registrarti subito!
+                    </a>";
+            }
+            else {
+                $password = md5($_POST["password"]);
+                $q2="SELECT * from utenti where email=$1 and password=$2";
+                $result = pg_query_params($q2, array($email, $password));
+                if (!($line=pg_fetch_array($result,null,PGSQL_ASSOC))) {
+                    echo "Password errata";
+                }
+                else {
+                    $nome=$line["nome"];
+                    echo "<a href=index.html?name=$nome> Premi qui </a> per iniziare a navigare!";
+
+                }
+            }
+        }
+
+    ?>
+    </body>
+</html>
