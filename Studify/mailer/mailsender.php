d82f4542ed86b2f549c768932b4272edf73cdf44
AlessandroDiPatria <82099379+AlessandroDiPatria@users.noreply.github.com>
2021-05-29 18:30:00 +0200

    nuova robba

---
+++ Studify/mailer/mailsender.php
@@ -0,0 +1,34 @@
+<?php
+ use PHPMailer\PHPMailer\PHPMailer;
+ use PHPMailer\PHPMailer\SMTP;
+ use PHPMailer\PHPMailer\Exception;
+ require 'vendor/autoload.php';
+ 
+function send_mail($email, $oggetto="Newsletter", $messaggio="Ciao", $path_allegato = null){
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->Host = "smtp.gmail.com."; //indirizzo del server di posta in uscita
+        $mail->SMTPDebug = 0;
+        $mail->Port = 587; //porta del server di posta in uscita
+ 	    $mail->SMTPAuth = true;
+        $mail->SMTPAutoTLS = false;
+	    $mail->SMTPSecure = 'tls'; //tls o ssl informarsi presso il provider del vostro server di posta
+        $mail->Username = "alessandro.dipatria@gmail.com"; //la vostra mail
+        $mail->Password = "105178gla"; //password per accedere alla vostra mail
+        $mail->Priority    = 1; //(1 = High, 3 = Normal, 5 = low)
+        $mail->setFrom('alessandro.dipatria@gmail.com', 'Alessandro'); //impostazione del mittente
+        $mail->AddAddress($email);
+        $mail->IsHTML(true); 
+        $mail->Subject  =  $oggetto;
+        $mail->Body     =  $messaggio;
+        $mail->AltBody  =  "";
+        $mail->AddAttachment($path_allegato);  
+        if(!$mail->Send()){
+                echo "errore nell'invio della mail: ".$mail->ErrorInfo;
+                return false;
+        }else{
+                return true;
+        }
+        //echo !extension_loaded('openssl')?"Not Available":"Available";
+}
+?>
